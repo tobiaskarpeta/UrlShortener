b9ced628eedcbde67e151d8239d4bd56ec7d5db7
tobiaskarpeta <tobiaskarpeta@protonmail.com>
2025-06-17 00:12:18 +0200

    Track url access count

--- UrlShortener.Api/Features/Urls/Queries/GetUrlByCodeQueryHandler.cs
+++ UrlShortener.Api/Features/Urls/Queries/GetUrlByCodeQueryHandler.cs
@@ -14,7 +14,15 @@ namespace UrlShortener.Api.Features.Urls.Queries
         public async Task<string?> Handle(GetUrlByCodeQuery request, CancellationToken cancellationToken)
         {
             var shortenedUrlRecord = await _repository.GetByShortCode(request.ShortCode, cancellationToken);
-            return shortenedUrlRecord?.OriginalUrl;
+            if (shortenedUrlRecord == null)
+            {
+                return null;
+            }
+
+            shortenedUrlRecord.AccessCount++;
+            await _repository.SaveChangesAsync(cancellationToken);
+            
+            return shortenedUrlRecord.OriginalUrl;
         }
     }
 }
